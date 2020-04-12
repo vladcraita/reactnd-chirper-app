4cd4420aea99aac1297cd87965713909456aa97c
Vlad Craita <vlad.craita@yahoo.com>
2020-04-12 20:28:18 +0300

    tweet and loading bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import {getInitialData} from "../utils/api";
 import {receiveTweets} from "./tweets";
 import {receiveUsers} from "./users";
 import {setAuthedUser} from "./authedUser";
+import {hideLoading, showLoading} from "react-redux-loading-bar";
 
 const AUTHED_ID = 'tylermcginnis';
 
 export function handleInitialData() {
     return (dispatch) => {
+        dispatch(showLoading());
         return getInitialData()
             .then(({users, tweets}) => {
                 dispatch(receiveUsers(users));
                 dispatch(receiveTweets(tweets));
                 dispatch(setAuthedUser(AUTHED_ID));
+                dispatch(hideLoading());
             });
     };
 }
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,18 +1,58 @@
 import React, {Component} from "react";
-import {formatTweet} from "../utils/helpers";
+import {formatDate, formatTweet} from "../utils/helpers";
 import {connect} from "react-redux";
+import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline} from 'react-icons/ti/index';
 
 class Tweet extends Component {
+    toParent = (e, id) => {
+        e.preventDefault();
+
+    }
+
+    handleLike = e => {
+        e.preventDefault();
+    }
+
     render() {
-        const {tweet} = this.props.tweet;
+        const {tweet} = this.props;
 
         if (tweet === null) {
             return <p>This tweet id doesn't exist</p>
         }
 
-        return (
-            <div className="tweet">
+        const {
+            name, avatar, timestamp, text, hasLiked, likes, replies, id, parent
+        } = tweet;
 
+        return (
+            <div className='tweet'>
+                <img
+                    src={avatar}
+                    alt={`Avatar of ${name}`}
+                    className='avatar'
+                />
+                <div className='tweet-info'>
+                    <div>
+                        <span>{name}</span>
+                        <div>{formatDate(timestamp)}</div>
+                        {parent && (
+                            <button className='replying-to' onClick={(e) => this.toParent(e, parent.id)}>
+                                Replying to @{parent.author}
+                            </button>
+                        )}
+                        <p>{text}</p>
+                    </div>
+                    <div className='tweet-icons'>
+                        <TiArrowBackOutline className='tweet-icon' />
+                        <span>{replies !== 0 && replies}</span>
+                        <button className='heart-button' onClick={this.handleLike}>
+                            {hasLiked === true
+                                ? <TiHeartFullOutline color='#e0245e' className='tweet-icon' />
+                                : <TiHeartOutline className='tweet-icon'/>}
+                        </button>
+                        <span>{likes !== 0 && likes}</span>
+                    </div>
+                </div>
             </div>
         );
     }
